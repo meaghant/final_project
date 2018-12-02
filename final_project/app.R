1ee220e1ce3f5042085462bf73a0334b1e6ced67
Meaghan Townsend <meaghan.e.townsend@gmail.com>
2018-12-02 12:57:35 -0500

    functional boxplots in app

--- final_project/app.R
+++ final_project/app.R
@@ -8,23 +8,50 @@ library(plotly)
 library(ggpubr)
 library(tidyverse)
 
-raw_data <- read_sav("33968-0001-Data.sav") %>%
-  select(ICPSR_CENTERID_PK, RDDAYWKLI_PK, TOYTOTAL_PK, PALS_PK, TOTALASQSCORE_PK) %>%
-  mutate(RDDAYWKLI_PK = as.numeric(RDDAYWKLI_PK),
-         TOYTOTAL_PK = as.numeric(TOYTOTAL_PK),
+raw_data <- read_sav("33968-0001-Data.sav")
+
+clean_data <- raw_data %>%
+  select(ICPSR_CENTERID_PK, RDDAYWKLI_PK, NUBOOKSLI_PK, EDUCAT_PK, PALS_PK, TOTALASQSCORE_PK, INCOMELI_PK) %>%
+  mutate(RDDAYWKLI_PK = as.character(RDDAYWKLI_PK),
+         RDDAYWKLI_PK = fct_recode(RDDAYWKLI_PK, "0" = "0",
+                                   "1" = "1",
+                                   "2" = "2",
+                                   "3" = "3",
+                                   "4" = "4",
+                                   "5" = "5",
+                                   "6" = "6",
+                                   "7" = "7"),
+         NUBOOKSLI_PK = as.character(NUBOOKSLI_PK),
+         NUBOOKSLI_PK = fct_recode(NUBOOKSLI_PK, "None" = "1",
+                                   "1 to 9 books" = "2",
+                                   "10 to 24 books" = "3",
+                                   "25 to 49 books" = "4",
+                                   "50 or more books" = "5"),
+         EDUCAT_PK = case_when(EDUCAT_PK %in% 1:2 ~ "High school or less",
+                                 INCOMELI_PK %in% 3:5 ~ "Some college/associate's or technical degree",
+                                 INCOMELI_PK %in% 6:7 ~ "Graduate degree"),
+         EDUCAT_PK = fct_relevel(EDUCAT_PK, c("High school or less", "Some college/associate's or technical degree", "Graduate degree")),
          PALS_PK = as.numeric(PALS_PK),
-         TOTALASQSCORE_PK = as.numeric(TOTALASQSCORE_PK)) %>%
+         TOTALASQSCORE_PK = as.numeric(TOTALASQSCORE_PK),
+         INCOMELI_PK = case_when(INCOMELI_PK %in% 1:5 ~ "Under $50,000",
+                                 INCOMELI_PK %in% 6:10 ~ "$50,000-100,000",
+                                 INCOMELI_PK %in% 11 ~ "$100,000-150,000",
+                                 INCOMELI_PK %in% 12 ~ "$150,000-200,000",
+                                 INCOMELI_PK %in% 13 ~ "Over $200,000"),
+         INCOMELI_PK = fct_relevel(INCOMELI_PK, c("Under $50,000", "$50,000-100,000", "$100,000-150,000", "$150,000-200,000", "Over $200,000")),
+         INCOMELI_PK = as_vector(INCOMELI_PK)) %>%
   filter(!is.na(RDDAYWKLI_PK),
-         !is.na(TOYTOTAL_PK),
+         !is.na(NUBOOKSLI_PK),
+         !is.na(EDUCAT_PK),
          !is.na(PALS_PK),
-         !is.na(TOTALASQSCORE_PK)) %>%
-  group_by(ICPSR_CENTERID_PK)
+         !is.na(TOTALASQSCORE_PK))
 
 characteristic_choices <- c("Number of Books Read Per Week" = "RDDAYWKLI_PK",
-                            "Number of Toys in Household" = "TOYTOTAL_PK")
+                            "Number of Books in Home" = "NUBOOKSLI_PK",
+                            "Maternal Education Level" = "EDUCAT_PK")
 
-outcome_choices <- c("PALS Academic Score" = "PALS_PK",
-                     "ASQ Behavioral Score" = "TOTALASQSCORE_PK")
+outcome_choices <- c("PALS Academic Performance Score" = "PALS_PK",
+                     "ASQ Behavioral Score (higher = worse behavior)" = "TOTALASQSCORE_PK")
 
 ui <- fluidPage(
    
@@ -56,8 +83,9 @@ server <- function(input, output) {
    
    output$distPlot <- renderPlot({
      
-     raw_data %>% 
-       ggplot(aes_string(x = input$characteristic, y = input$outcome)) + geom_col()
+     clean_data %>% 
+       group_by(ICPSR_CENTERID_PK) %>%
+       ggplot(aes_string(x = input$characteristic, y = input$outcome)) + geom_boxplot() 
      
    })
 }
