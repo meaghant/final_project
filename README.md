ff2a4b4d8687f6304ecd12f9fa2a96567242702d
Meaghan Townsend <meaghan.e.townsend@gmail.com>
2018-12-02 16:12:20 -0500

    updated app and created readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# final_project
+# Final Project: What Effect Do Non-School Factors have on Preschoolers' Academic and Social Outcomes?
+
+For this project, I examine data from the Massachusetts Early Care and Education and School Readiness Study, which assesses the specific factors inside and outside of infant and preschool classrooms that promote school readiness.
+
+Although early childhood education plays a tremendously important role in shaping a child's academic and social development, I was most interested in factors outside the classroom. I chose to examine the non-school factors of number of books in the home, maternal education level, and family income level. I also chose to focus on preschool-age children rather than infants or toddlers.
+
+I hypothesized that preschool children who (1) are exposed to a greater number of books in their homes, (2) have mothers with a higher degree of education, and (3) come from higher-income households will have better literacy and social development outcomes. To gauge literacy outcomes, I chose the Phonological Awareness and Literacy Screening (PALS) score. To gauge behavioral outcomes, I chose the Ages and Stages Questionnaire (ASQ) Score for social development. 
+
+I thank you for taking the time to examine this project and hope you find it rewarding. Below please find the link to my app:
+
+https://meaghant.shinyapps.io/final_project/
+
--- final_project/app.R
+++ final_project/app.R
@@ -56,93 +56,93 @@ outcome_choices <- c("PALS Litearcy Score" = "PALS_PK",
 
 ui <- fluidPage(
   
-   # I chose a dark navy theme and titled my app.
+  # I chose a dark navy theme and titled my app.
   
-   theme = shinytheme("superhero"),
-   
-   titlePanel("What Effect Do Non-School Factors have on Preschoolers' Academic and Social Outcomes?"),
-   
-   # I created a sidebar with two drop-down menus. The first allows the user to select one of three non-school factors; the second allows the user to select one of two outcome variables.
-   
-   sidebarLayout(
-     sidebarPanel(
-       selectInput("nonschool",
-                   "Select a non-school factor:",
-                   choices = nonschool_choices,
-                   selected = "Number of Books Read Per Week"),
+  theme = shinytheme("superhero"),
+  
+  titlePanel("What Effect Do Non-School Factors have on Preschoolers' Literacy and Social Development Outcomes?"),
+  
+  # I created a sidebar with two drop-down menus. The first allows the user to select one of three non-school factors; the second allows the user to select one of two outcome variables.
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("nonschool",
+                  "Select a non-school factor:",
+                  choices = nonschool_choices,
+                  selected = "Number of Books Read Per Week"),
       selectInput("outcome",
                   "Select an outcome variable:",
-                   choices = outcome_choices,
-                   selected = "PALS Academic Score")
-     ),
-      
-      # In the main panel, I created several tabs to keep my app looking clean: an "About" tab to introduce the app, an "Explore" tab for the data, a "Takeaways" tab to share my findings, and a "Learn more" tab for anyone interested in doing so.
-      # I wrote up my interpretations of the data and printed it beneath the boxplot in the "Explore" tab.
-      
-     mainPanel(
-        tabsetPanel(type = "tabs",
-                    tabPanel("About this app", htmlOutput("about")),
-                    tabPanel("Explore the data", 
-                             plotOutput("boxplot"),
-                             h3("Interpretation of Findings"),
-                             p("My hypotheses largely held true. Across the board, there was a stronger correlation between the non-school factor and the literacy outcome variable than with the social development outcome variable. This is perhaps surprising, given that all of these factors were removed from the classroom."),
-                             h3("Number of Books Read Per Week"),
-                             p("The number of books in the home was positively correlated with literacy outcomes. Preschoolers who came from 50+ book households outscored peers from households with fewer than 9 books by 1.5 standard deviations on the PALS literacy score."),
-                             p("A greater number of books was also correlated, though not as strongly, with improved social development. There were, however, a notable number of poorly-behaved children from households with many books, suggesting that exposure to books does not prevent the instance of poor behavior."),
-                             h3("Maternal Education Level"),
-                             p("Maternal education level had an even stronger positive correlation with improved literacy outcomes. The spread of the data was consistent between educational levels, but the average literacy score improved with each additional level of school completed by the student’s mother."),
-                             p("Children of mothers who completed only high school or less were significantly less well behaved than children of other mothers. However, there were minimal differences in the social development of children whose mothers completed some college or received an associate’s, technical, bachelor’s or graduate degree."),
-                             h3("Family Income Level"),
-                             p("There was also a slight positive correlation between family income level and literacy outcomes. The largest gap was between families with incomes below $50,000 and all other families, whose children all exhibited similar literacy scores."),
-                             p("Finally, children of parents from lower-income families performed less well on scores of social development. Again, a gap emerged between families living on less than $50,000, whose children had the worst social development scores, and other families, whose children performed similarly to each other. Six of the seven worst-behaved children were in the lowest income bracket."),
-                             h3("Implications"),
-                             p("Above a certain threshold – whether number of books in the home, maternal education level, or family income level – children seem to perform at roughly the same level on literacy and social development outcomes. The substantial drop-offs in performance seen at the lowest levels of privilege on each factor might incline policymakers to focus non-school reform efforts towards those most in need.")),
-                    tabPanel("Learn more", htmlOutput("learn")))
-      )
-   )
+                  choices = outcome_choices,
+                  selected = "PALS Academic Score")
+    ),
+    
+    # In the main panel, I created several tabs to keep my app looking clean: an "About" tab to introduce the app, an "Explore" tab for the data, a "Takeaways" tab to share my findings, and a "Learn more" tab for anyone interested in doing so.
+    # I wrote up my interpretations of the data and printed it beneath the boxplot in the "Explore" tab.
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("About this app", htmlOutput("about")),
+                  tabPanel("Explore the data", 
+                           plotOutput("boxplot"),
+                           h3("Interpretation of Findings"),
+                           p("My hypotheses largely held true. Across the board, there was a stronger correlation between the non-school factor and the literacy outcome variable than with the social development outcome variable. This is perhaps surprising, given that all of these factors were removed from the classroom."),
+                           h3("Number of Books Read Per Week"),
+                           p("The number of books in the home was positively correlated with literacy outcomes. Preschoolers who came from 50+ book households outscored peers from households with fewer than 9 books by 1.5 standard deviations on the PALS literacy score."),
+                           p("A greater number of books was also correlated, though not as strongly, with improved social development. There were, however, a notable number of poorly-behaved children from households with many books, suggesting that exposure to books does not prevent the instance of poor behavior."),
+                           h3("Maternal Education Level"),
+                           p("Maternal education level had an even stronger positive correlation with improved literacy outcomes. The spread of the data was consistent between educational levels, but the average literacy score improved with each additional level of school completed by the student’s mother."),
+                           p("Children of mothers who completed only high school or less were significantly less well behaved than children of other mothers. However, there were minimal differences in the social development of children whose mothers completed some college or received an associate’s, technical, bachelor’s or graduate degree."),
+                           h3("Family Income Level"),
+                           p("There was also a slight positive correlation between family income level and literacy outcomes. The largest gap was between families with incomes below $50,000 and all other families, whose children all exhibited similar literacy scores."),
+                           p("Finally, children of parents from lower-income families performed less well on scores of social development. Again, a gap emerged between families living on less than $50,000, whose children had the worst social development scores, and other families, whose children performed similarly to each other. Six of the seven worst-behaved children were in the lowest income bracket."),
+                           h3("Implications"),
+                           p("Above a certain threshold – whether number of books in the home, maternal education level, or family income level – children seem to perform at roughly the same level on literacy and social development outcomes. The substantial drop-offs in performance seen at the lowest levels of privilege on each factor might incline policymakers to focus non-school reform efforts towards those most in need.")),
+                  tabPanel("Learn more", htmlOutput("learn")))
+    )
+  )
 )
 
 # I defined my server logic, producing a different output for each tab.
 
 server <- function(input, output) {
   
-   # For the "about" tab, I pasted together strings of text describing my project. I created headers and subtext to make it more readable.
+  # For the "about" tab, I pasted together strings of text describing my project. I created headers and subtext to make it more readable.
   
-   output$about <- renderUI({
+  output$about <- renderUI({
     
     str1 <- paste("Welcome")
     str2 <- paste("For this project, I examine data from the Massachusetts Early Care and Education and School Readiness Study, which assesses the specific factors inside and outside of infant and preschool classrooms that promote school readiness.")
     str3 <- paste("My Focus")
-    str4 <- paste("I chose to examine the non-school factors of number of books in the home, maternal education level, and family income level. To gauge literacy outcomes, I chose the Phonological Awareness and Literacy Screening (PALS) score. To gauge behavioral outcomes, I chose the Stages Questionnaire (ASQ) Score for social development. These variables can all be selected on the left sidebar.")
+    str4 <- paste("I chose to examine the non-school factors of number of books in the home, maternal education level, and family income level. To gauge literacy outcomes, I chose the Phonological Awareness and Literacy Screening (PALS) score. To gauge behavioral outcomes, I chose the Ages and Stages Questionnaire (ASQ) Score for social development. These variables can all be selected on the left sidebar.")
     str5 <- paste("Hypotheses")
     str6 <- paste("I hypothesized that preschool children who (1) are exposed to a greater number of books in their homes, (2) have mothers with a higher degree of education, and (3) come from higher-income households will have better literacy and social development outcomes.")
     
     HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6)))
   })
-   
-   # For the data tab, I piped the cleaned data into a boxplot of outcome vs. non-school factors. I chose to visualize these data with boxplots to communicate the spread of each variable. Again drawing from the solutions to problem set 7, I used aes_string instead of aes because selectInput stored both nonschool and outcome as strings. I also grouped by the unique child ID and rotated the x-axis labels by 30 degrees to make them more readable.
-   
-   output$boxplot <- renderPlot({
-     
-     clean_data %>% 
-       group_by(ICPSR_CENTERID_PK) %>%
-       ggplot(aes_string(x = input$nonschool, y = input$outcome)) + geom_boxplot() +
-       theme(axis.text.x = element_text(angle = 30, hjust = 1))
-
-   })
-   
-   # For the "learn more" tab, I pasted together strings of text describing my project. I created headers and subtext to make it more readable.
-   
-   output$learn <- renderUI({
-     
-     str1 <- paste("About the Massachusetts Early Care and Education and School Readiness Study")
-     str2 <- paste("This study was conducted in Massachusetts from 2001 to 2008 by Wellesley College researchers. The study collected data at 12, 24, and 36 months, and in the year before kindergarten. I examine only the pre-kindergarten year data. The average age of the children in the study at this time was 59.79 months, or 4.98 years, with a standard deviation of 4.10 months. Of the 248 families originally recruited for the study, 233 remain. This represents an attrition rate of 3.6%.")
-     str3 <- paste("The study was directed by Wendy Wagner Robeson, Ed.D., Joanne Roberts, Ph.D., and Nancy L. Marshall, Ed.D. Funding was provided by (1) the United States Department of Health and Human Services, Administration for Children and Families, Office of Planning, Research and Evaluation; (2) the United States Department of Health and Human Services, Administration for Children and Families, Office of Child Care; and (3) the Harold Benenson Memorial Research Fund.")
-     str4 <- paste("Citation")
-     str5 <- paste("Marshall, Nancy, Roberts, Joanne, and Wagner Robeson, Wendy. Massachusetts Early Care and Education and School Readiness Study, 2001-2008. Ann Arbor, MI: Inter-university Consortium for Political and Social Research [distributor], 2013-04-05. https://doi.org/10.3886/ICPSR33968.v1.")
-     
-     HTML(paste(h3(str1), p(str2), p(str3), h3(str4), p(str5)))
-   })
+  
+  # For the data tab, I piped the cleaned data into a boxplot of outcome vs. non-school factors. I chose to visualize these data with boxplots to communicate the spread of each variable. Again drawing from the solutions to problem set 7, I used aes_string instead of aes because selectInput stored both nonschool and outcome as strings. I also grouped by the unique child ID and rotated the x-axis labels by 30 degrees to make them more readable.
+  
+  output$boxplot <- renderPlot({
+    
+    clean_data %>% 
+      group_by(ICPSR_CENTERID_PK) %>%
+      ggplot(aes_string(x = input$nonschool, y = input$outcome)) + geom_boxplot() +
+      theme(axis.text.x = element_text(angle = 30, hjust = 1))
+    
+  })
+  
+  # For the "learn more" tab, I pasted together strings of text describing my project. I created headers and subtext to make it more readable.
+  
+  output$learn <- renderUI({
+    
+    str1 <- paste("About the Massachusetts Early Care and Education and School Readiness Study")
+    str2 <- paste("This study was conducted in Massachusetts from 2001 to 2008 by Wellesley College researchers. The study collected data at 12, 24, and 36 months, and in the year before kindergarten. I examine only the pre-kindergarten year data. The average age of the children in the study at this time was 59.79 months, or 4.98 years, with a standard deviation of 4.10 months. Of the 248 families originally recruited for the study, 233 remain. This represents an attrition rate of 3.6%.")
+    str3 <- paste("The study was directed by Wendy Wagner Robeson, Ed.D., Joanne Roberts, Ph.D., and Nancy L. Marshall, Ed.D. Funding was provided by (1) the United States Department of Health and Human Services, Administration for Children and Families, Office of Planning, Research and Evaluation; (2) the United States Department of Health and Human Services, Administration for Children and Families, Office of Child Care; and (3) the Harold Benenson Memorial Research Fund.")
+    str4 <- paste("Citation")
+    str5 <- paste("Marshall, Nancy, Roberts, Joanne, and Wagner Robeson, Wendy. Massachusetts Early Care and Education and School Readiness Study, 2001-2008. Ann Arbor, MI: Inter-university Consortium for Political and Social Research [distributor], 2013-04-05. https://doi.org/10.3886/ICPSR33968.v1.")
+    
+    HTML(paste(h3(str1), p(str2), p(str3), h3(str4), p(str5)))
+  })
 }
 
 # Run the application 
